c879ebe03b440f366a4186f91ad9220e0be648f5
Breno Campos <40044002+InOrbitOut@users.noreply.github.com>
2018-07-12 21:20:30 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const http = require('http');
 const port = process.env.PORT || 8080;
 
 http.createServer(function(req,res) {
-    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    res.end('<h1>Hello, World!</h1>');
+    var array = req.url.split("/");
+    
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
+    res.end(req.url
+           + "\n" + array);
 }).listen(port);
